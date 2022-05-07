f43fc5c98f11b95f1138f80c01b9269ce198f199
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-05-07 19:29:54 +0300

    feat: add model slides in file js (Sat, May 07, 2022 19:29:54)

--- cssMemSlider/js/main.js
+++ cssMemSlider/js/main.js
@@ -1,1 +1,11 @@
-console.log('HI')
+const slides = [
+    {src: './img/slide_1.1.jpg', alt: 'slide_1', title: 'Fort on the lake'},
+    {src: './img/slide_2.1.jpg', alt: 'slide_2', title: 'West coast'},
+    {src: './img/slide_3.1.jpg', alt: 'slide_3', title: 'New Your'},
+    {src: './img/slide_4.1.jpg', alt: 'slide_4', title: 'UK Real Estate'},
+    {src: './img/slide_5.1.jpg', alt: 'slide_5', title: 'River in the forest'},
+    {src: './img/slide_6.1.jpg', alt: 'slide_6', title: 'Grass-plot'},
+];
+
+
+console.log('HI', slides)
